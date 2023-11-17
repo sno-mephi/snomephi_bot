0af838e584194036438707b00776a2a76a0f7134
Ivan <idfedorov09@gmail.com>
2023-11-17 20:59:14 +0300

    add isAllowed in textCommands

--- src/main/java/ru/idfedorov09/telegram/bot/data/enums/TextCommands.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/enums/TextCommands.kt
@@ -1,12 +1,34 @@
 package ru.idfedorov09.telegram.bot.data.enums
 
-enum class TextCommands(val text: String) {
-    TEST_COMMAND_1("Тестовая команда 1"),
-    TEST_COMMAND_2("Тестовая команда 2"),
+import ru.idfedorov09.telegram.bot.data.model.UserActualizedInfo
+
+enum class TextCommands(
+
+    /** текст команды **/
+    private val text: String,
+
+    /** роли которым доступна эта команда **/
+    private val allowedRoles: List<UserRole> = listOf(UserRole.USER),
+) {
+    TEST_COMMAND_1(
+        "Тестовая команда 1",
+        listOf(UserRole.ROOT),
+    ),
+
+    TEST_COMMAND_2(
+        "Тестовая команда 2",
+        listOf(UserRole.ROOT),
+    ),
+
     ;
 
     /** Проверяет, является ли текст командой **/
     companion object {
         fun isTextCommand(text: String) = entries.map { it.text }.contains(text)
+    }
+
+    fun isAllowed(user: UserActualizedInfo): Boolean {
+        if (user.roles.contains(UserRole.ROOT)) return true
+        return user.roles.map { this.allowedRoles.contains(it) }.firstOrNull { it } ?: false
     }
 }
