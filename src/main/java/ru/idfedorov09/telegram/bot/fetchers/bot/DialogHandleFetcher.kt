3ad955fff145c2907cdb954e7ddd1ef27a782cf2
Ivan <idfedorov09@gmail.com>
2023-11-23 22:08:35 +0300

    dialog done!

--- src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/DialogHandleFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/DialogHandleFetcher.kt
@@ -1,9 +1,14 @@
 package ru.idfedorov09.telegram.bot.fetchers.bot
 
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import ru.idfedorov09.telegram.bot.data.enums.QuestionStatus
+import ru.idfedorov09.telegram.bot.data.enums.TextCommands
+import ru.idfedorov09.telegram.bot.data.model.Quest
 import ru.idfedorov09.telegram.bot.data.model.QuestDialogMessage
+import ru.idfedorov09.telegram.bot.data.model.User
 import ru.idfedorov09.telegram.bot.data.model.UserActualizedInfo
 import ru.idfedorov09.telegram.bot.executor.Executor
 import ru.idfedorov09.telegram.bot.repo.QuestDialogMessageRepository
@@ -42,6 +47,19 @@ class DialogHandleFetcher(
         val responder = userRepository.findById(quest.responderId!!).get()
         val isByQuestionAuthor = author.tui == userActualizedInfo.tui
 
+        val params = Params(
+            messageText = messageText,
+            quest = quest,
+            author = author,
+            responder = responder,
+        )
+
+        // если пришла команда - обрабатываем как команду, остальное скипаем
+        if (TextCommands.isTextCommand(messageText)) {
+            handleCommands(params)
+            return
+        }
+
         val questDialogMessage = QuestDialogMessage(
             questId = quest.id,
             isByQuestionAuthor = isByQuestionAuthor,
@@ -60,4 +78,44 @@ class DialogHandleFetcher(
             },
         )
     }
+
+    private fun handleCommands(params: Params) {
+        when (params.messageText) {
+            TextCommands.QUEST_DIALOG_CLOSE.commandText -> closeDialog(params)
+        }
+    }
+
+    private fun closeDialog(params: Params) {
+        questRepository.save(
+            params.quest.copy(
+                questionStatus = QuestionStatus.CLOSED,
+            ),
+        )
+
+        bot.execute(
+            SendMessage().also {
+                it.chatId = params.author.tui!!
+                it.text = "_⚠\uFE0F Оператор завершил диалог\\._"
+                it.parseMode = ParseMode.MARKDOWNV2
+            },
+        )
+
+        bot.execute(
+            SendMessage().also {
+                it.chatId = params.responder.tui!!
+                it.text = "Спасибо за обратную связь\\! *Диалог завершен\\.*"
+                it.parseMode = ParseMode.MARKDOWNV2
+            },
+        )
+    }
+
+    /**
+     * Вспомогательный класс для передачи параметров
+     */
+    private data class Params(
+        val messageText: String,
+        val quest: Quest,
+        val author: User,
+        val responder: User,
+    )
 }
