2c512a151872a690fff3d0c3bf26044d073ca87e
Anastasia Viktorova <viktorovaa.04@gmail.com>
2023-11-25 17:42:50 +0300

    добавлено описание к LastUserActionType добавлены название и описание к UserRole добавлены команды USER_INFO, ROLE_DESCRIPTION в TextCommands RoleDescriptionFetcher UserInfoFetcher

---
+++ src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/UserInfoFetcher.kt
@@ -0,0 +1,130 @@
+package ru.idfedorov09.telegram.bot.fetchers.bot
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+import ru.idfedorov09.telegram.bot.data.enums.TextCommands
+import ru.idfedorov09.telegram.bot.data.model.UserActualizedInfo
+import ru.idfedorov09.telegram.bot.executor.Executor
+import ru.idfedorov09.telegram.bot.repo.UserRepository
+import ru.idfedorov09.telegram.bot.util.UpdatesUtil
+import ru.mephi.sno.libs.flow.belly.InjectData
+import ru.mephi.sno.libs.flow.fetcher.GeneralFetcher
+
+@Component
+class UserInfoFetcher(
+    private val updatesUtil: UpdatesUtil,
+    private val userRepository: UserRepository,
+) : GeneralFetcher() {
+    companion object {
+        private val log = LoggerFactory.getLogger(UserInfoFetcher::class.java)
+    }
+
+    @InjectData
+    fun doFetch(
+        update: Update,
+        bot: Executor,
+        userActualizedInfo: UserActualizedInfo,
+    ) {
+        val chatId = updatesUtil.getChatId(update) ?: return
+        val messageText = updatesUtil.getText(update) ?: return
+
+        val params = UserInfoFetcher.Params(
+            messageText = messageText,
+            userActualizedInfo = userActualizedInfo,
+            update = update,
+            chatId = chatId,
+            bot = bot,
+        )
+
+        if (messageText.contains(TextCommands.USER_INFO.commandText)) {
+            handleCommands(params)
+        }
+    }
+
+    private fun handleCommands(params: UserInfoFetcher.Params) {
+        if (!params.messageText.contains(TextCommands.USER_INFO.commandText)) return
+
+        if (TextCommands.USER_INFO.commandText in params.messageText) {
+            if (TextCommands.USER_INFO.isAllowed(params.userActualizedInfo)) {
+                val messageSplit = params.messageText.split(" ", "\n", "\t")
+                when (messageSplit.size) {
+                    1 -> params.bot.execute(
+                        SendMessage(
+                            params.chatId,
+                            "отправьте команду формата" +
+                                " \"/userinfo tui\"",
+                        ),
+                    )
+                    else -> {
+                        val tui = messageSplit[1]
+                        sendUserInfo(params, tui)
+                    }
+                }
+            } else {
+                params.bot.execute(SendMessage(params.chatId, "Нет прав"))
+            }
+        }
+    }
+
+    private fun sendUserInfo(params: Params, tui: String) {
+        val user = userRepository.findByTui(tui)
+        if (user == null) {
+            params.bot.execute(SendMessage(params.chatId, "Пользователь не найден"))
+            return
+        }
+
+        val fullName = user.fullName ?: "-"
+        val studyGroup = user.studyGroup ?: "-"
+        val lastTgNick = user.lastTgNick ?: "-"
+        val id = user.id ?: "-"
+        // записать в нормальной форме
+        val lastUserActionType = when (user.lastUserActionType) {
+            null -> "-"
+            else -> user.lastUserActionType.actionDescription
+        }
+
+        var userCategories = ""
+        if (user.categories.isNotEmpty()) {
+            for (s in user.categories) {
+                userCategories += String.format("\n-%s", s)
+            }
+        } else {
+            userCategories = "\nпусто"
+        }
+
+        var userRoles = ""
+        if (user.roles.isNotEmpty()) {
+            for (s in user.roles) {
+                userRoles += String.format("\n-%s", s.roleName)
+            }
+        } else {
+            userRoles = "\nпусто"
+        }
+
+        val msg = SendMessage()
+        msg.chatId = params.chatId
+        msg.text = String.format(
+            "\uD83D\uDC64ФИО: %s\n\uD83D\uDCDAгруппа: %s\n\n\uD83D\uDD11роли:%s\n\n\uD83D\uDCF1последний ник в tg: " +
+                "%s\n\n\uD83D\uDDD2категории:%s\n\ntui: %s\nid: %s\n\nпоследнее действие: %s",
+            fullName,
+            studyGroup,
+            userRoles,
+            lastTgNick,
+            userCategories,
+            tui,
+            id,
+            lastUserActionType,
+        )
+        params.bot.execute(msg)
+    }
+
+    private data class Params(
+        val messageText: String,
+        val update: Update,
+        val userActualizedInfo: UserActualizedInfo,
+        val chatId: String,
+        val bot: Executor,
+    )
+}
