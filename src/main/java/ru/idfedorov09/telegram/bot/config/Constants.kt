e1491ac5904f2742df25ab078d69e50ee9975525
tanexc <artur.231456@gmail.com>
2023-11-27 00:07:42 +0300

    strings for registration messages moved to enums

--- src/main/java/ru/idfedorov09/telegram/bot/config/Constants.kt
+++
@@ -1,9 +0,0 @@
-package ru.idfedorov09.telegram.bot.config
-
-const val REGISTRATION_FULL_NAME_REQUEST = "Пожалуйста, введите свое ФИО"
-const val REGISTRATION_GROUP_REQUEST = "Введите свою группу"
-const val REGISTRATION_INVALID_GROUP = "Кажется такой группы не существует, пожалуйста, введите свою группу заново"
-const val REGISTRATION_COMPLETE = "Спасибо, Вы зарегистрированы"
-const val REGISTRATION_WELCOME = "Здравствуйте"
-const val REGISTRATION_INVALID_FULL_NAME = "Кажется вы ввели ФИО неправильно"
-const val REGISTRATION_START = "Здравствуйте! Вы не зарегистрированы"
