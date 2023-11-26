c059d5cd3f049937bcb706a8a5d37eb8b352ba11
tanexc <artur.231456@gmail.com>
2023-11-27 00:08:47 +0300

    user fullName and group validator edit

--- src/main/java/ru/idfedorov09/telegram/bot/util/Validator.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/util/Validator.kt
@@ -1,7 +1,9 @@
 package ru.idfedorov09.telegram.bot.util
 
-fun String?.isValidFullName() = this?.isNotEmpty() ?: false
+fun String?.isValidFullName() = this?.let {
+    it.isNotEmpty() && it.length < 128
+} ?: false
 
 fun String?.isValidGroup() = this?.let {
-    it.isNotEmpty() && "([МСБмсб]{1})([0-9]{2})-([0-9]{3})".toRegex().matches(it)
+    it.isNotEmpty() && "([АМСБамсб]{1})([0-9]{2})-([0-9]{3})".toRegex().matches(it)
 } ?: false
