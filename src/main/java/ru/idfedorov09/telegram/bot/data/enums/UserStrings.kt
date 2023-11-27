497f0abd1c9664b161c493686169b78648a12c34
tanexc <artur.231456@gmail.com>
2023-11-27 03:48:34 +0300

    strings for registration edit

--- src/main/java/ru/idfedorov09/telegram/bot/data/enums/UserStrings.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/enums/UserStrings.kt
@@ -4,15 +4,20 @@ enum class UserStrings(private val text: String) {
     FullNameRequest("Пожалуйста, введите свое ФИО"),
     GroupRequest("Введите свою группу"),
 
+    FullNameConfirmation("Вы действительно хотите использовать ФИО %s?"),
+    GroupConfirmation("Вы действительно состоите в группе %s?"),
+
     InvalidGroup("Кажется такой группы не существует, пожалуйста, введите свою группу заново"),
-    InvalidFullName("Кажется вы ввели ФИО неправильно"),
-    AlreadyExists("Такой пользователь уже зарегистрирован"),
+    InvalidFullName("Кажется Вы ввели ФИО неправильно"),
+    AlreadyExists("Такой пользователь уже зарегистрирован под юзернеймом @%s"),
 
     RegistrationComplete("Спасибо, Вы зарегистрированы"),
-    Welcome("Здравствуйте"),
+    Welcome("Здравствуйте, %s"),
     RegistrationStart("Здравствуйте! Вы не зарегистрированы");
 
 
     operator fun invoke(extraText: String = "") = text + extraText
+
+    fun format(parameter: String) = text.format(parameter)
 
 }
