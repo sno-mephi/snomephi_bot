24b5401c2acac4afa333dc66b939ddb1f5af2642
vSEK1RO <vladlitvinov2006@gmail.com>
2023-12-05 15:58:44 +0300

    Category pagination

--- src/main/java/ru/idfedorov09/telegram/bot/repo/CategoryRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/repo/CategoryRepository.kt
@@ -1,6 +1,13 @@
 package ru.idfedorov09.telegram.bot.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import ru.idfedorov09.telegram.bot.data.model.Category
 
-interface CategoryRepository : JpaRepository<Category, Long>
+interface CategoryRepository : JpaRepository<Category, Long>{
+    @Query(value = "SELECT u FROM category_table u LIMIT u.pageSize OFFSET u.page",
+        nativeQuery = true)
+    fun findCategoriesByPage(@Param("page")page: Long, @Param("pageSize")pageSize: Long): List<Category>
+}
