2e9e7c2a596da8a7c9c461ce38a6136caf9b0080
andromedagalua <andromedagalua@yandex.ru>
2024-03-03 21:47:17 +0300

    Закончил BroadcastService

--- src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
@@ -2,6 +2,10 @@ package ru.idfedorov09.telegram.bot.util
 
 import org.jvnet.hk2.annotations.Service
 import org.springframework.scheduling.annotation.Scheduled
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
+import org.telegram.telegrambots.meta.api.objects.InputFile
+import ru.idfedorov09.telegram.bot.executor.Executor
 import ru.idfedorov09.telegram.bot.repo.BroadcastRepository
 import ru.idfedorov09.telegram.bot.repo.UserRepository
 
@@ -9,15 +13,38 @@ import ru.idfedorov09.telegram.bot.repo.UserRepository
 class BroadcastSenderService(
     private val broadcastRepository: BroadcastRepository,
     private val userRepository: UserRepository,
+    private val bot: Executor,
 ) {
     @Scheduled(fixedDelay = 1000)
     fun broadcastSender() {
         val firstActiveBroadcast = broadcastRepository.findFirstActiveBroadcast() ?: return
-        userRepository.findAll().forEach{
-            if (it.id !in firstActiveBroadcast.receivedUsersId) {
-
-            }
-            }
+        val firstUser = userRepository.findAll().firstOrNull {
+            it.id !in firstActiveBroadcast.receivedUsersId &&
+                (
+                    it.categories.intersect(firstActiveBroadcast.categoriesId).isEmpty() ||
+                        firstActiveBroadcast.categoriesId.isEmpty()
+                    )
+        } ?: run {
+            broadcastRepository.save(
+                firstActiveBroadcast.copy(isCompleted = true),
+            )
+            return
         }
+        if (firstActiveBroadcast.imageHash == null) {
+            bot.execute(
+                SendMessage().also {
+                    it.chatId = firstUser.tui.toString()
+                    it.text = firstActiveBroadcast.text.toString()
+                },
+            )
+        } else {
+            bot.execute(
+                SendPhoto().also {
+                    it.chatId = firstUser.tui.toString()
+                    it.photo = InputFile(firstActiveBroadcast.imageHash)
+                },
+            )
+        }
+        firstUser.id?.let { firstActiveBroadcast.receivedUsersId.add(it) }
     }
 }
