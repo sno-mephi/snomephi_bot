7d96ae755217a6204328d9909c7870309570c9db
andromedagalua <andromedagalua@yandex.ru>
2024-03-03 22:50:48 +0300

    Добавил кнопки в BroadcastSenderService

--- src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
@@ -5,15 +5,21 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
 import org.telegram.telegrambots.meta.api.objects.InputFile
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import ru.idfedorov09.telegram.bot.data.model.Broadcast
 import ru.idfedorov09.telegram.bot.executor.Executor
 import ru.idfedorov09.telegram.bot.repo.BroadcastRepository
+import ru.idfedorov09.telegram.bot.repo.ButtonRepository
 import ru.idfedorov09.telegram.bot.repo.UserRepository
 
 @Service
 class BroadcastSenderService(
     private val broadcastRepository: BroadcastRepository,
     private val userRepository: UserRepository,
+    private val buttonRepository: ButtonRepository,
     private val bot: Executor,
+
 ) {
     @Scheduled(fixedDelay = 1000)
     fun broadcastSender() {
@@ -35,6 +41,7 @@ class BroadcastSenderService(
                 SendMessage().also {
                     it.chatId = firstUser.tui.toString()
                     it.text = firstActiveBroadcast.text.toString()
+                    it.replyMarkup = createChooseKeyboard(firstActiveBroadcast)
                 },
             )
         } else {
@@ -42,9 +49,26 @@ class BroadcastSenderService(
                 SendPhoto().also {
                     it.chatId = firstUser.tui.toString()
                     it.photo = InputFile(firstActiveBroadcast.imageHash)
+                    it.replyMarkup = createChooseKeyboard(firstActiveBroadcast)
                 },
             )
         }
         firstUser.id?.let { firstActiveBroadcast.receivedUsersId.add(it) }
+    }
+    private fun createKeyboard(keyboard: List<List<InlineKeyboardButton>>) =
+        InlineKeyboardMarkup().also { it.keyboard = keyboard }
+    private fun createChooseKeyboard(firstActiveBroadcast: Broadcast): InlineKeyboardMarkup {
+        val keyboardList = mutableListOf<List<InlineKeyboardButton>>()
+        buttonRepository.findAllById(firstActiveBroadcast.buttonsId).forEach {button ->
+            keyboardList.add(
+                listOf(
+                    InlineKeyboardButton("${button.text}").also {
+                        it.url = button.link
+                        it.callbackData = button.callbackData
+                    }
+                ),
+            )
+        }
+        return createKeyboard(keyboardList)
     }
 }
