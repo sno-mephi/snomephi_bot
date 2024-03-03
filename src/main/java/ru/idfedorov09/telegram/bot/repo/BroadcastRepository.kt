8bf35cf0fc6f606ca7c7d37ecab18d9664b08b30
andromedagalua <andromedagalua@yandex.ru>
2024-03-03 20:58:32 +0300

    начал писать сервис

--- src/main/java/ru/idfedorov09/telegram/bot/repo/BroadcastRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/repo/BroadcastRepository.kt
@@ -1,6 +1,21 @@
 package ru.idfedorov09.telegram.bot.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import ru.idfedorov09.telegram.bot.data.model.Broadcast
 
-interface BroadcastRepository : JpaRepository<Broadcast, Long>
+interface BroadcastRepository : JpaRepository<Broadcast, Long> {
+    @Query(
+        """
+            SELECT * 
+            FROM broadcast_table
+            WHERE 1=1
+                AND is_completed IS false
+                AND CURRENT_TIMESTAMP >= broadcast_start_dttm AT TIME ZONE 'Europe/Moscow'
+                AND is_built IS true
+            LIMIT 1
+        """,
+        nativeQuery = true,
+    )
+    fun findFirstActiveBroadcast(): Broadcast?
+}
