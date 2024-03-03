7fa5e90ba958c15008362a4bd7607f8db488d92b
andromedagalua <andromedagalua@yandex.ru>
2024-03-03 20:17:11 +0300

    добавил репозиторий

--- src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
@@ -1,4 +1,12 @@
 package ru.idfedorov09.telegram.bot.util
 
+import org.jvnet.hk2.annotations.Service
+import org.springframework.scheduling.annotation.Scheduled
+
+@Service
 class BroadcastSenderService {
+    @Scheduled(fixedDelay = 1000)
+    fun broadcastSender(){
+
+    }
 }
