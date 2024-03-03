56aac0caf15d8ea3b8bcf294a7d1f498de9b470a
andromedagalua <andromedagalua@yandex.ru>
2024-03-03 20:58:32 +0300

    начал писать сервис

--- src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/util/BroadcastSenderService.kt
@@ -2,11 +2,22 @@ package ru.idfedorov09.telegram.bot.util
 
 import org.jvnet.hk2.annotations.Service
 import org.springframework.scheduling.annotation.Scheduled
+import ru.idfedorov09.telegram.bot.repo.BroadcastRepository
+import ru.idfedorov09.telegram.bot.repo.UserRepository
 
 @Service
-class BroadcastSenderService {
+class BroadcastSenderService(
+    private val broadcastRepository: BroadcastRepository,
+    private val userRepository: UserRepository,
+) {
     @Scheduled(fixedDelay = 1000)
-    fun broadcastSender(){
+    fun broadcastSender() {
+        val firstActiveBroadcast = broadcastRepository.findFirstActiveBroadcast() ?: return
+        userRepository.findAll().forEach{
+            if (it.id !in firstActiveBroadcast.receivedUsersId) {
 
+            }
+            }
+        }
     }
 }
