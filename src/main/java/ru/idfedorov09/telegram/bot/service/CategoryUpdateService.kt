5dcf5a2c8c7574e87d4e88e112d46f96321ee281
andromedagalua <andromedagalua@yandex.ru>
2024-03-16 21:53:00 +0300

    сделал https://www.notion.so/31ef19e760674e38a9ddc47a19fc1e00

---
+++ src/main/java/ru/idfedorov09/telegram/bot/service/CategoryUpdateService.kt
@@ -0,0 +1,34 @@
+package ru.idfedorov09.telegram.bot.service
+
+import org.springframework.stereotype.Service
+import ru.idfedorov09.telegram.bot.data.model.Category
+import ru.idfedorov09.telegram.bot.repo.CategoryRepository
+import ru.idfedorov09.telegram.bot.repo.UserRepository
+
+/**
+    Сервис для изменения категорий юзеров
+ */
+@Service
+class CategoryUpdateService (
+    private val userRepository: UserRepository,
+    private val categoryRepository: CategoryRepository,
+) {
+    fun switchCategory(
+        userId: Long?,
+        categoryId: Long,
+    ) {
+        categoryRepository.findById(categoryId) ?: return
+        val user = userId?.let { userRepository.findById(it).get() } ?: return
+        if (user.categories.contains(categoryId)) {
+            userRepository.removeCategory(
+                categoryId = categoryId,
+                userId = userId,
+            )
+        } else (
+           userRepository.addCategory(
+               categoryId = categoryId,
+               userId = userId,
+           )
+        )
+    }
+}
