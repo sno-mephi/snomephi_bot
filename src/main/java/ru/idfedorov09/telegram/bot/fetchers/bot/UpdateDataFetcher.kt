875336f5420c03ae4fc59e83ef4e7851f6b389c8
andromedagalua <andromedagalua@yandex.ru>
2024-04-10 15:02:28 +0300

    сделал https://www.notion.so/Actions-4c5d3b2bfe5049e7a4dbe027d4d4a68c

--- src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/UpdateDataFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/UpdateDataFetcher.kt
@@ -1,19 +1,25 @@
 package ru.idfedorov09.telegram.bot.fetchers.bot
 
+import jakarta.persistence.Column
+import jakarta.persistence.Convert
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery
 import org.telegram.telegrambots.meta.api.objects.Update
+import ru.idfedorov09.telegram.bot.data.enums.LastUserActionType
 import ru.idfedorov09.telegram.bot.data.enums.QuestionStatus
 import ru.idfedorov09.telegram.bot.data.model.QuestDialog
 import ru.idfedorov09.telegram.bot.data.model.User
+import ru.idfedorov09.telegram.bot.data.model.UserAction
 import ru.idfedorov09.telegram.bot.data.model.UserActualizedInfo
+import ru.idfedorov09.telegram.bot.data.model.converter.LastUserActionTypeConverter
 import ru.idfedorov09.telegram.bot.executor.Executor
 import ru.idfedorov09.telegram.bot.fetchers.DefaultFetcher
-import ru.idfedorov09.telegram.bot.repo.BanRepository
-import ru.idfedorov09.telegram.bot.repo.BroadcastRepository
-import ru.idfedorov09.telegram.bot.repo.QuestDialogRepository
-import ru.idfedorov09.telegram.bot.repo.UserRepository
+import ru.idfedorov09.telegram.bot.repo.*
+import ru.idfedorov09.telegram.bot.util.UpdatesUtil
 import ru.mephi.sno.libs.flow.belly.InjectData
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import kotlin.jvm.optionals.getOrNull
 
 /**
@@ -25,7 +31,9 @@ class UpdateDataFetcher(
     private val questDialogRepository: QuestDialogRepository,
     private val broadcastRepository: BroadcastRepository,
     private val banRepository: BanRepository,
-    private val bot: Executor
+    private val userActionRepository: UserActionRepository,
+    private val bot: Executor,
+    private val updatesUtil: UpdatesUtil,
 ) : DefaultFetcher() {
     @InjectData
     fun doFetch(
@@ -36,12 +44,14 @@ class UpdateDataFetcher(
             bot.execute(AnswerCallbackQuery(update.callbackQuery.id))
         }
         when {
-            userActualizedInfo != null -> updateUser(userActualizedInfo)
+            userActualizedInfo != null -> updateUser(userActualizedInfo, update)
         }
     }
 
-    private fun updateUser(userActualizedInfo: UserActualizedInfo) {
+    private fun updateUser(userActualizedInfo: UserActualizedInfo, update: Update) {
         userActualizedInfo.apply {
+            val lastUserActionTypeFromRepository = id?.let { userRepository.findById(it).get().lastUserActionType }
+            if (lastUserActionTypeFromRepository != lastUserActionType) newUserAction(userActualizedInfo, update)
             userRepository.save(
                 User(
                     id = id,
@@ -67,6 +77,28 @@ class UpdateDataFetcher(
             banData?.let {
                 banRepository.save(it)
             }
+        }
+    }
+
+    private fun newUserAction(userActualizedInfo: UserActualizedInfo, update: Update) {
+        userActualizedInfo.apply {
+            val callbackId = if (update.callbackQuery?.data?.contains(Regex("\"\\\\d\"")) == true) {
+                update.callbackQuery?.data!!.toLong()
+            } else null
+            userActionRepository.save(
+                UserAction(
+                    lastUserActionType = lastUserActionType,
+                    actionTime = updatesUtil.getDate(update)
+                        ?.let { Instant.ofEpochSecond(it).atZone(ZoneId.of("Europe/Moscow")).toLocalDateTime() },
+                    userId = id,
+                    tui = tui,
+                    messageText = update.message?.text,
+                    callbackDataId = callbackId,
+                    callbackDataLegacy = callbackId?.let { _ ->
+                        update.callbackQuery?.data
+                    }
+                )
+            )
         }
     }
 
