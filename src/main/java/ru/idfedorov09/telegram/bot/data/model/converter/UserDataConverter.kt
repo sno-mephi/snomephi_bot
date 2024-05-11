195a0cb6ac4b24d62341fe96d95afbceaa630585
andromedagalua <andromedagalua@yandex.ru>
2024-05-11 20:26:01 +0300

    Revert "ошибка обработана, но измененнная инфа не сохраняется в таблицу"
    This reverts commit ba73f63bc4eee327ea859bed6e6e63cb6d1d6ec1.

--- src/main/java/ru/idfedorov09/telegram/bot/data/model/converter/UserDataConverter.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/model/converter/UserDataConverter.kt
@@ -1,6 +1,5 @@
 package ru.idfedorov09.telegram.bot.data.model.converter
 
-import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.persistence.*
 import ru.idfedorov09.telegram.bot.data.model.UserData
@@ -14,10 +13,6 @@ class UserDataConverter : AttributeConverter<UserData, String> {
     }
 
     override fun convertToEntityAttribute(dbData: String?): UserData? {
-        return try {
-            dbData?.let { objectMapper.readValue(it, UserData::class.java) }
-        } catch (e: JsonProcessingException) {
-            UserData(dataLegacy = dbData)
-        }
+        return dbData?.let { objectMapper.readValue(it, UserData::class.java) }
     }
 }
