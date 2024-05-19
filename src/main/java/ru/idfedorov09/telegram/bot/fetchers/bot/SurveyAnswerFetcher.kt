601a0717e2a12750f3a098d568ac2f166275b486
andromedagalua <andromedagalua@yandex.ru>
2024-05-20 00:17:53 +0300

    Закончил конструктор

--- src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/SurveyAnswerFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetchers/bot/SurveyAnswerFetcher.kt
@@ -3,14 +3,17 @@ package ru.idfedorov09.telegram.bot.fetchers.bot
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.Update
 import ru.idfedorov09.telegram.bot.annotation.FetcherPerms
+import ru.idfedorov09.telegram.bot.data.enums.CallbackCommands
 import ru.idfedorov09.telegram.bot.data.enums.UserRole
 import ru.idfedorov09.telegram.bot.data.model.UserActualizedInfo
 import ru.idfedorov09.telegram.bot.fetchers.DefaultFetcher
+import ru.idfedorov09.telegram.bot.repo.CallbackDataRepository
 import ru.mephi.sno.libs.flow.belly.InjectData
+import kotlin.jvm.optionals.getOrNull
 
 @Component
 class SurveyAnswerFetcher (
-
+    private val callbackDataRepository: CallbackDataRepository,
 ) : DefaultFetcher() {
     @InjectData
     @FetcherPerms(UserRole.MAILER)
@@ -31,7 +34,15 @@ class SurveyAnswerFetcher (
     }
 
     private fun callbackQueryHandler(params: Params) {
+        val callbackId = params.update.callbackQuery.data?.toLongOrNull()
+        callbackId ?: return
+        val callbackData = callbackDataRepository.findById(callbackId).getOrNull() ?: return
 
+        callbackData.callbackData?.apply {
+            when {
+                startsWith(CallbackCommands.SURVEY_USER_ANSWER.data)
+            }
+        }
     }
 
     private data class Params(
